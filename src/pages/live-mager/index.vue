e34383e528acca53273e840a73f601372a794d41
yongguan.yang <yongguan.yang@vhall.com>
2018-08-13 18:18:06 +0800

    ‘’

---
+++ src/pages/live-mager/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="live-mager">
+    <div class="live-title">
+      <span>直播列表</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index'
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "~assets/css/variable";
+  .live-mager{
+    background-color: $color-bg;
+    border-radius: 5px;
+    overflow: hidden;
+    .live-title{
+      line-height: 50px;
+      border-bottom: 1px solid $color-bd;
+      span{
+        display: inline-block;
+        margin-left: 20px;
+      }
+    }
+  }
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,19 +3,27 @@ const Entry = () =>
   import('src/pages/interact/entry')
 const Test = () =>
   import('src/pages/com-test')
+const LiveMager = () =>
+  import('src/pages/live-mager')
 
-export default [{
-  path: '/',
-  redirect: '/entry'
-},
-{
-  path: '/entry',
-  name: 'Entry',
-  component: Entry
-},
-{
-  path: '/test',
-  name: 'Test',
-  component: Test
-}
+export default [
+  {
+    path: '/',
+    redirect: '/entry'
+  },
+  {
+    path: '/entry',
+    name: 'Entry',
+    component: Entry
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: Test
+  },
+  {
+    path: '/liveMager',
+    name: 'liveMager',
+    component: LiveMager
+  }
 ]
