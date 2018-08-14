5b4b9b1197196011ef017db2a4e7be21fb9069c3
quchan <chan.qu@vhall.com>
2018-08-14 20:17:11 +0800

    '删除input组件中的console'

--- src/pages/login/set-password.vue
+++ src/pages/login/set-password.vue
@@ -3,14 +3,15 @@
     <p>
       感谢您选择微吼，在开始前请先设置登录密码
     </p>
-    <com-input :value.sync="outValue" placeholder="请输入公司名称" :max-length="50" ></com-input>
+    <com-input :value.sync="outValue" placeholder="" ></com-input>
+    <p>密码创建要求</p>
+    <a href="javascript:;" class="v-confirm" @click="setPassword()">确定</a>
   </div>
 </template>
 <script>
   export default {
     data () {
       return {
-        items: ['account', 'mobile'],
         outValue: ''
       }
     },
@@ -21,6 +22,9 @@
     watch: {
     },
     methods: {
+      setPassword () {
+        alert(this.outValue)
+      }
     }
   }
 </script>
@@ -30,5 +34,15 @@
   height: 600px;
   margin: 200px auto;
   border: 1px solid #333;
+  .v-confirm {
+    display: block;
+    width: 385px;
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    background-color: #333;
+    color: #fff;
+    text-decoration: none;
+  }
 }
 </style>
