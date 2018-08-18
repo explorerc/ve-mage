3779fa3cba8f288555d99d3aab4c1e94d218b201
jinpeng <peng.jin@vhall.com>
2018-08-18 18:03:54 +0800

    update

--- src/pages/live-mager/prepare/limit/com-invited.vue
+++ src/pages/live-mager/prepare/limit/com-invited.vue
@@ -22,11 +22,30 @@
     <transition name='fade'>
       <div class="modal-cover" v-if='modalShow' @click="closeModal">
         <div class='modal-box'>
-          <h2>选择观众组 <span>×</span></h2>
+          <h4>选择观众组 <span>×</span></h4>
+          <div>
           <com-tabs :value.sync="tabValue">
-            <com-tab label="用户管理" :index="1">用户管理的内容详情</com-tab>
-            <com-tab label="定时任务补偿" :index="2">定时任务补偿的内容详情</com-tab>
+            <com-tab label="分组" :index="1">分组</com-tab>
+            <com-tab label="标签" :index="2">标签</com-tab>
           </com-tabs>
+          <div class="right">
+            <div class='top'>
+              <span>分组:<i>123</i>个</span>
+              <span><com-input :value.sync="searchTitle" placeholder="请输入关键字" :max-length="60" ></com-input>搜索</span>
+            </div>
+            <ol class='list'>
+              <li><span><i class='icon'></i>企业员工<i>(123)</i></span><em class='choose'>选择</em></li>
+              <li><span><i class='icon'></i>企业员工<i>(123)</i></span><em class='choose'>选择</em></li>
+              <li><span><i class='icon'></i>企业员工<i>(123)</i></span><em class='choose'>选择</em></li>
+              <li><span><i class='icon'></i>企业员工<i>(123)</i></span><em class='choose'>选择</em></li>
+              <li><span><i class='icon'></i>企业员工<i>(123)</i></span><em class='choose'>选择</em></li>
+            </ol>
+            <div class='btm clearfix'>
+              <span>已选择:<i>啊啊啊</i><i>啊啊啊</i><i>啊啊啊</i></span>
+              <el-button>确定</el-button>
+            </div>
+          </div>
+          </div>
         </div>
       </div>
     </transition>
@@ -38,7 +57,8 @@
     data () {
       return {
         modalShow: false,
-        tabValue: 1
+        tabValue: 1,
+        searchTitle: ''
       }
     },
     methods: {
@@ -52,6 +72,8 @@
 </script>
 
 <style lang='scss' scoped>
+@import '~assets/css/variable';
+@import '~assets/css/base';
 .group {
   span {
     display: inline-block;
@@ -75,17 +97,52 @@
 }
 
 .modal-box {
-  width: 400px;
-  height: 300px;
+  width: 700px;
+  height: 500px;
   position: fixed;
   top: 50%;
   left: 50%;
-  margin-top: -150px;
-  margin-left: -200px;
+  margin-top: -250px;
+  margin-left: -350px;
   background: white;
   border-radius: 10px;
   border: 1px solid #666;
   padding: 30px;
   text-align: center;
+  .top {
+  }
+  .btm {
+    span {
+      float: left;
+    }
+    button {
+      float: right;
+    }
+    i {
+      display: inline-block;
+      padding: 5px 10px;
+      border: 1px solid #ccc;
+      margin: 0 3px;
+    }
+  }
+}
+.list {
+  li {
+    height: 50px;
+    line-height: 50px;
+    i.icon {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      border-radius: 100px;
+      border: 1px solid #ccc;
+    }
+    span {
+      float: left;
+    }
+    em {
+      float: right;
+    }
+  }
 }
 </style>
