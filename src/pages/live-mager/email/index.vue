11927144742158d3561008dc556f43bfde4219a4
yongguan.yang <yongguan.yang@vhall.com>
2018-08-23 16:06:32 +0800

    邮件邀约

--- src/pages/live-mager/email/index.vue
+++ src/pages/live-mager/email/index.vue
@@ -35,10 +35,10 @@
         <el-table-column
           label="操作">
           <template slot-scope="scope">
-            <el-button type="text" size="small" @click.stop="queryInfoEmail(scope.$index)">查看</el-button>
-            <el-button type="text" size="small" @click.stop="sendEmail(scope.$index)">立刻发送</el-button>
-            <el-button type="text" size="small" @click.stop="editEmail(scope.$index)">编辑</el-button>
-            <el-button type="text" size="small" @click.stop="delEmail(scope.$index)">删除</el-button>
+            <el-button type="text" size="small" @click.stop="clickEmail(scope.$index,handleType.info)">查看</el-button>
+            <el-button type="text" size="small" @click.stop="clickEmail(scope.$index,handleType.send)">立刻发送</el-button>
+            <el-button type="text" size="small" @click.stop="clickEmail(scope.$index,handleType.edit)">编辑</el-button>
+            <el-button type="text" size="small" @click.stop="clickEmail(scope.$index,handleType.delete)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -51,16 +51,32 @@
         </div>
       </div>
     </div>
+    <message-box
+      v-if="sendShow"
+      header="提示"
+      content="活动删除后，所有数据将一并删除，并且不可恢复。确定要删除吗？"
+      cancelText="取消"
+      confirmText='删除'
+      @handleClick="handleClickSendEmail">
+      5666666666666666666
+    </message-box>
   </div>
 </template>
 
 <script>
   import LiveHttp from 'src/api/activity-manger'
   import VePagination from 'src/components/ve-pagination'
+  import MessageBox from 'src/components/common/message-box/com'
 
+  const handleType = {
+    info: 'queryInfoEmail',
+    send: 'sendEmail',
+    edit: 'editEmail',
+    delete: 'deleteEmail'
+  }
   export default {
     name: 'index',
-    components: {VePagination},
+    components: {VePagination, MessageBox},
     data () {
       return {
         isInvite: false,
@@ -68,6 +84,9 @@
         total: 0,
         currentPage: 1,
         liveId: '', // 活动id
+        sendShow: false,
+        currentEmailIdx: '',
+        handleType: handleType,
         emailList: [
           {id: 0, title: '111', sendTime: '2019-10-22', sendCount: 10, state: '草稿'},
           {id: 1, title: '22', sendTime: '2019-10-22', sendCount: 10, state: '草稿'},
@@ -108,16 +127,39 @@
           console.log(e)
         })
       },
-      queryInfoEmail (idx) {
-        const emaiId = this.emailList[idx].id
+      clickEmail (idx, type) {
+        this.currentEmailIdx = idx
+        if (type === this.handleType.info) { // 查看
+          this.queryInfoEmail()
+        } else if (type === this.handleType.edit) { // 编辑
+          this.editEmail()
+        } else if (type === this.handleType.send) { // 立即发送
+          this.sendShow = true
+        } else if (type === this.handleType.delete) { // 删除
+          this.$messageBox({
+            header: '',
+            content: '删除邮件后，邮件内容将无法找回，确认删除？',
+            cancelText: '取消',
+            confirmText: '删除',
+            handleClick: (e) => {
+              debugger
+              if (e.action === 'confirm') {
+                this.delEmail()
+              }
+            }
+          })
+        }
+      },
+      queryInfoEmail () {
+        const emaiId = this.emailList[this.currentEmailIdx].id
         LiveHttp.queryEmailInfoById(emaiId).then((res) => {
         }).catch((e) => {
           console.log('查询邮件信息失败')
           console.log(e)
         })
       },
-      sendEmail (idx) {
-        const emaiId = this.emailList[idx].id
+      sendEmail () {
+        const emaiId = this.emailList[this.currentEmailIdx].id
         LiveHttp.sendEmailInfo(emaiId).then((res) => {
           if (res.code === 200) {
             console.log('邮件发送成功')
@@ -127,8 +169,15 @@
           console.log(e)
         })
       },
-      delEmail (idx) {
-        const emaiId = this.emailList[idx].id
+      handleClickSendEmail (e) {
+        console.log(e)
+        this.sendShow = false
+        if (e.action === 'confirm') {
+          this.sendEmail()
+        }
+      },
+      delEmail () {
+        const emaiId = this.emailList[this.currentEmailIdx].id
         LiveHttp.deleteEmailById(emaiId).then((res) => {
           if (res.code === 200) {
             this.queryEmailListById()
@@ -138,8 +187,8 @@
           console.log(e)
         })
       },
-      editEmail (idx) {
-        const emaiId = this.emailList[idx].id
+      editEmail () {
+        const emaiId = this.emailList[this.currentEmailIdx].id
         this.$router.push(`/liveMager/emailEdit/${this.liveId}?email=${emaiId}`)
       },
       addEmail () {
