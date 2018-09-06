5e2c89bff6f4a2a35deba155ccd7e37f06809f5b
xiaoguang.hu <xiaoguang.hu@vhall.com>
2018-09-06 22:06:17 +0800

    commit

---
+++ build2/webpack.prod.conf.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const config = require('./config')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+function subPath (_path) {
+  return path.posix.join(config[process.env.BUILD_ENV].SUB_DIR, _path)
+}
+
+module.exports = {
+  output: {
+    filename: subPath('js/[name].[chunkhash:8].js'),
+    chunkFilename: subPath('js/[name].[chunkhash:8].js')
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: subPath('css/[name].css'),
+      chunkFilename: subPath('css/[name].[contenthash:5].css')
+    }),
+    new CleanWebpackPlugin(['dist'], {
+      root: resolve('/'),
+      verbose: true
+    })
+  ]
+}
