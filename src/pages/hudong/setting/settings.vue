bb16c3068eb283c26a9c7bb6cc9d50f4119cdf32
yongguan.yang <yongguan.yang@vhall.com>
2018-09-10 15:13:37 +0800

    直播设置

--- src/pages/hudong/setting/settings.vue
+++ src/pages/hudong/setting/settings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="setting-box">
+  <div class="setting-box setting-box-select">
     <div class="form-list">
       <div class="form-row">
         <div class="form-title">推流方式</div>
@@ -10,10 +10,16 @@
           </div>
         </div>
       </div>
-      <div class="form-row">
+      <div class="form-row" v-if="!setting.pushStreamType">
         <div class="form-title">媒体设备</div>
         <div class="form-content">
-          <button class="default-button fr">选择设备</button>
+          <button class="default-button fr" @click="deviceShow = true">选择设备</button>
+        </div>
+      </div>
+      <div class="form-row" v-else>
+        <div class="form-title">推流设置</div>
+        <div class="form-content">
+          <button class="default-button fr" @click="pushStreamShow = true">推流设置</button>
         </div>
       </div>
       <div class="form-row">
@@ -48,7 +54,7 @@
               </el-option>
             </el-select>
             内增加
-            <com-input value="30"></com-input>
+            <com-input :value.sync="addPersonCount"></com-input>
             人
           </p>
           <p class="btn-box">
@@ -58,14 +64,113 @@
         </div>
       </transition>
     </div>
+    <!-- 媒体设置 -->
+    <message-box
+      v-if="deviceShow"
+      type="prompt"
+      header="媒体设置"
+      confirmText="确定"
+      width="442px"
+      @handleClick="deviceMsgClick">
+      <div class="form-list form-message-box device-box">
+        <div class="video-box" id="videoBoxId"></div>
+        <div class="form-row">
+          <div class="form-title fzr">摄像头</div>
+          <div class="form-content">
+            <el-select v-model="addPersonTime" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-title fzr">麦克风</div>
+          <div class="form-content">
+            <el-select v-model="addPersonTime" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-title fzr">扬声器</div>
+          <div class="form-content">
+            <el-select v-model="addPersonTime" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+          </div>
+        </div>
+        <p class="update-device fzr">刷新设备</p>
+      </div>
+    </message-box>
+    <!-- 推流设置 -->
+    <message-box
+      v-if="pushStreamShow"
+      type="prompt"
+      header="推流设置"
+      confirmText="确定"
+      width="660px"
+      @handleClick="handleMsgClick">
+      <div class="form-list form-message-box">
+        <h3 style="text-align: center;">正在使用第三方推流工具发起直播</h3>
+        <div class="form-row">
+          <div class="form-title fzr">RTMP URL</div>
+          <div class="form-content">
+            <com-input :value.sync="rtmpUrl"></com-input>
+            <button class="default-button" @click="copyInput(0)">复制</button>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-title fzr">串流码/播放路径</div>
+          <div class="form-content">
+            <com-input :value.sync="streamPlayUrl"></com-input>
+            <button class="default-button" @click="copyInput(1)">复制</button>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-title fzr">使用场景：</div>
+          <div class="form-content">
+            大会直播、活动拍摄等使用OBS、VMIX等第三方推流软件进行直播的场景
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="form-title fzr">使用步骤：</div>
+          <div class="form-content">
+            <p>1.在第三方推流工具中添加RTMP URL与串流码。</p>
+            <p style="line-height: 10px;">2.使用第三方推流工具进行推流。</p>
+            <p>3.在右上角点击开始直播按钮。</p>
+          </div>
+        </div>
+      </div>
+    </message-box>
   </div>
 </template>
 
 <script>
+  // import HostPusher from 'src/components/common/video/push/HostPusher'
+
   export default {
     name: 'settings',
     data () {
       return {
+        pushStreamShow: false,
+        deviceShow: false,
+        addPersonCount: '30',
+        rtmpUrl: '',
+        streamPlayUrl: '',
         setting: {
           isChart: false, // 是否聊天禁言
           isPersonCount: false, // 是否在线人数
@@ -93,13 +198,42 @@
     methods: {
       changeNav (idx) {
         this.setting.pushStreamType = idx
+      },
+      handleMsgClick (e) {
+        this.pushStreamShow = false
+      },
+      deviceMsgClick (e) {
+        this.deviceShow = false
+      },
+      copyInput (type) {
+        const copyStr = type ? this.streamPlayUrl : this.rtmpUrl
+        copyStr.copyClipboard(() => {
+          this.$toast({
+            header: `提示`,
+            content: '复制成功',
+            autoClose: 1000,
+            position: 'top-center'
+          })
+        })
       }
     }
   }
 </script>
-
+<style lang="scss">
+  .setting-box-select {
+    .el-select {
+      .el-input__inner {
+        border: solid 1px #e5e5e5;
+      }
+    }
+    .ve-message-box__wrapper .ve-message-box .ve-message-box__btns {
+      text-align: center;
+    }
+  }
+</style>
 <style lang="scss" scoped>
   @import "assets/css/mixin.scss";
+
   .setting-box {
     width: 100%;
     max-width: 500px;
@@ -110,6 +244,7 @@
     font-size: 14px;
     background-color: $color-bg-sub;
   }
+
   .nav-menu {
     font-size: 0;
     border: solid 1px $color-bd;
@@ -131,6 +266,7 @@
       }
     }
   }
+
   .form-list {
     overflow: hidden;
     .form-row {
@@ -139,6 +275,7 @@
       padding: 10px 20px;
       .form-title {
         width: 140px;
+        padding-right: 10px;
       }
       .form-content /deep/ {
         flex: 1;
@@ -149,13 +286,14 @@
       }
     }
   }
+
   .person-count-setting {
     position: relative;
     padding: 20px;
     border-top: solid 1px $color-bd;
     border-bottom: solid 1px $color-bd;
     background-color: #fff;
-    &:after{
+    &:after {
       display: block;
       position: absolute;
       content: '';
@@ -178,7 +316,7 @@
           line-height: 30px;
         }
       }
-      .el-select{
+      .el-select {
         width: 100px;
       }
     }
@@ -187,6 +325,49 @@
       margin: 20px 0 10px 0;
       button {
         margin: 0 10px;
+      }
+    }
+  }
+
+  .form-message-box {
+    margin-top: 20px;
+    .form-row {
+      padding: 10px 0;
+      .form-title {
+        width: 120px;
+      }
+      .com-input {
+        width: 310px;
+      }
+      .default-button {
+        margin-left: 10px;
+      }
+    }
+  }
+
+  .device-box /deep/ {
+    margin-top: 10px;
+    .form-row {
+      .form-title {
+        width: 54px;
+        text-align: left;
+      }
+      .el-select {
+        width: 348px;
+      }
+    }
+    .video-box {
+      width: 402px;
+      height: 200px;
+      margin: 0 auto 10px auto;
+      background-color: #8E9198;
+    }
+    .update-device {
+      text-align: right;
+      color: $color-blue;
+      &:hover {
+        cursor: pointer;
+        color: $color-blue-hover;
       }
     }
   }
