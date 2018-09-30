3ef64d6f2447519980ebd03f2aa687cfb58f01c7
yongguan.yang <yongguan.yang@vhall.com>
2018-09-30 10:30:28 +0800

    build test

--- src/components/common/loading/fun.js
+++ src/components/common/loading/fun.js
@@ -10,7 +10,7 @@ export default (options = {}) => {
   if (options === false) {
     state = false
   } else {
-    ;({ loadingText, target } = options)
+    ;({loadingText, target} = options)
   }
 
   if (!instance && state) {
--- src/pages/live-mager/promote/wechat/edit.vue
+++ src/pages/live-mager/promote/wechat/edit.vue
@@ -161,8 +161,8 @@
         },
         loading: false,
         searchPerson: '',
-        personList: [{ id: '', name: '', count: 0, isChecked: false }],
-        selectedPersonList: [{ id: '', name: '', count: 0, isChecked: false }],
+        personList: [{id: '', name: '', count: 0, isChecked: false}],
+        selectedPersonList: [{id: '', name: '', count: 0, isChecked: false}],
         selectedPersonListStr: '',
         selectPersonShow: false,
         selectedCount: 0,
@@ -177,7 +177,7 @@
     created () {
       this.queryPersonList()
       if (this.inviteId) {
-        this.$config({ loading: true }).$get(noticeService.GET_QUERY_WECHAT, {
+        this.$config({loading: true}).$get(noticeService.GET_QUERY_WECHAT, {
           inviteId: this.inviteId
         }).then((res) => {
           this.titleValue = res.data.title
@@ -223,7 +223,7 @@
               position: 'center'
             })
             // 跳转到列表页面
-            this.$router.push({ name: 'promoteWechat', params: { id: this.activityId } })
+            this.$router.push({name: 'promoteWechat', params: {id: this.activityId}})
           })
         })
       },
