dfa303d50244aca80397b8ad48a42dd696298249
xiaoguang.hu <xiaoguang.hu@vhall.com>
2018-10-08 20:53:09 +0800

    m

--- src/pages/site/index.vue
+++ src/pages/site/index.vue
@@ -101,7 +101,7 @@ export default {
   },
   methods: {
     init () {
-      this.$config({loading: true}).$get(brandService.GET_SITE_DATA, {
+      this.$config({ loading: true }).$get(brandService.GET_SITE_DATA, {
         activityId: this.$route.params.id
       }).then(res => {
         if (res.data.enabled === 'Y') {
@@ -167,7 +167,7 @@ export default {
     },
     useTemplate (temp) {
       let temData = defaultData[temp]()
-      this.$config({loading: true}).$post(brandService.POST_UPDATE_SITE, {
+      this.$config({ loading: true }).$post(brandService.POST_UPDATE_SITE, {
         activityId: this.$route.params.id,
         template: JSON.stringify(temData)
       }).then(res => {
