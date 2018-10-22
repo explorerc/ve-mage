8612b6919bbbd46d9964c03ca7afe547b6e073c1
yongguan.yang <yongguan.yang@vhall.com>
2018-10-22 19:11:54 +0800

    数据中心页面开发

--- src/utils/chart-tool.js
+++ src/utils/chart-tool.js
@@ -2,10 +2,23 @@ let echarts = require('echarts/lib/echarts')
 require('echarts/lib/chart/bar')
 require('echarts/lib/chart/line')
 require('echarts/lib/chart/pie')
+require('echarts/lib/chart/scatter')
 require('echarts/lib/component/tooltip')
 require('echarts/lib/component/legend')
 
+export function random (minVal, maxVal) {
+  minVal = minVal || 0
+  maxVal = maxVal || 100
+  return Math.round(Math.random() * maxVal) + minVal
+}
+
 const lineColor = '#e2e2e2'
+const grid = {
+  left: '2%',
+  right: '1%',
+  bottom: '1%',
+  top: '2%'
+}
 const AxisValue = {
   type: 'value',
   splitLine: {
@@ -94,13 +107,7 @@ export function barPile (id, data) {
       right: 10,
       data: data.legendData
     },
-    grid: {
-      left: '1%',
-      right: '2%',
-      top: '4%',
-      bottom: '3%',
-      containLabel: true
-    },
+    grid: grid,
     xAxis: AxisValue,
     yAxis: {
       ...AxisCategory,
@@ -155,10 +162,7 @@ export function lines (id, data) {
       itemHeight: 10
     },
     grid: {
-      left: '2%',
-      right: '0',
-      top: '4%',
-      bottom: '3%',
+      ...grid,
       containLabel: true
     },
     xAxis: {
@@ -244,7 +248,7 @@ export function pie (id, data) {
 }
 
 /***
- * 柱图
+ * 柱图圆形柱子
  * @returns {Promise<Response>}
  */
 export function barRadius (id, data) {
@@ -336,6 +340,285 @@ export function barRadius (id, data) {
         data: barData
       }
     ]
+  }
+  let myChart = echarts.init(document.getElementById(id))
+  myChart.setOption(option)
+  return myChart
+}
+
+/***
+ * 普通柱图
+ * @returns {Promise<Response>}
+ */
+export function bars (id, data, gridData) {
+  let xAxisData = []
+  let barData = []
+  data.forEach(item => {
+    xAxisData.push(item.name)
+    barData.push(item.value)
+  })
+  let option = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      },
+      textStyle: {
+        fontSize: 12
+      }
+    },
+    label: {
+      normal: {
+        textStyle: {
+          color: '#ff3b00'
+        }
+      },
+      emphasis: {
+        textStyle: {
+          color: '#ff3b00'
+        }
+      }
+    },
+    grid: {
+      ...grid,
+      ...gridData
+    },
+    xAxis: {
+      ...AxisCategory,
+      axisTick: {
+        show: true,
+        alignWithLabel: true
+      },
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: lineColor,
+          width: 1,
+          type: 'dashed'
+        }
+      },
+      data: xAxisData
+    },
+    yAxis: {
+      ...AxisValue,
+      splitArea: {
+        show: true,
+        areaStyle: {
+          color: ['#fafff9', '#fff']
+        }
+      },
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: lineColor,
+          width: 1,
+          type: 'dashed'
+        }
+      },
+      axisTick: {
+        show: true
+      }
+    },
+    series: [
+      {
+        type: 'bar',
+        barWidth: '50%',
+        itemStyle: {
+          normal: {
+            barBorderRadius: [10, 10, 0, 0],
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+              offset: 0,
+              color: 'rgba(253,156,65,0.9)'
+            }, {
+              offset: 1,
+              color: 'rgba(249,109,0,0.9)'
+            }])
+          },
+          emphasis: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+              offset: 0,
+              color: 'rgba(255,231,25,0.9)'
+            }, {
+              offset: 1,
+              color: 'rgba(250,182,0,0.9)'
+            }])
+          }
+        },
+        data: barData
+      }
+    ]
+  }
+  let myChart = echarts.init(document.getElementById(id))
+  myChart.setOption(option)
+  return myChart
+}
+
+/***
+ * 柱图，折线图组合图
+ * @returns {Promise<Response>}
+ */
+export function barAndLine (id, data, gridData) {
+  console.log(data)
+  let legendData = []
+  let seriesData = []
+  data.list.forEach(item => {
+    legendData.push(item.name)
+    if (item.type === 'bar') {
+      item.itemStyle = {
+        barBorderRadius: [5, 5, 0, 0]
+      }
+    } else {
+      item.lineStyle = {
+        color: 'red'
+      }
+    }
+    seriesData.push(item)
+  })
+  let option = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        crossStyle: {
+          color: '#999'
+        }
+      }
+    },
+    toolbox: {
+      feature: {
+        dataView: {show: true, readOnly: false},
+        magicType: {show: true, type: ['line', 'bar']},
+        restore: {show: true},
+        saveAsImage: {show: true}
+      }
+    },
+    legend: {
+      top: -5,
+      right: 10,
+      data: legendData
+    },
+    grid: {
+      ...grid,
+      ...gridData
+    },
+    xAxis: [
+      {
+        ...AxisCategory,
+        axisTick: {
+          show: true,
+          alignWithLabel: true
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: lineColor,
+            width: 1,
+            type: 'dashed'
+          }
+        },
+        data: data.xAxis
+      }
+    ],
+    yAxis: {
+      ...AxisValue,
+      splitArea: {
+        show: true,
+        areaStyle: {
+          color: ['#fafff9', '#fff']
+        }
+      },
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: lineColor,
+          width: 1,
+          type: 'dashed'
+        }
+      },
+      axisTick: {
+        show: true
+      }
+    },
+    series: seriesData
+  }
+  let myChart = echarts.init(document.getElementById(id))
+  myChart.setOption(option)
+  return myChart
+}
+
+/***
+ * 散点图
+ * @returns {Promise<Response>}
+ */
+export function scatter (id, datas, gridData) {
+  let maxValue = 0
+  datas.data.forEach(item => {
+    maxValue = maxValue > item[2] ? maxValue : item[2]
+  })
+  console.log('scatter-maxValue=' + maxValue)
+  let option = {
+    title: {
+      text: 'Punch Card of Github',
+      link: 'https://github.com/pissang/echarts-next/graphs/punch-card'
+    },
+    legend: {
+      data: ['观看时长'],
+      left: 'right'
+    },
+    tooltip: {
+      position: 'top',
+      formatter: function (params) {
+        return `${params.name}(${datas.yAxis[params.data[1]]})<br/>${params.seriesName}：${params.data[2]}秒`
+      }
+    },
+    grid: {
+      ...grid,
+      ...gridData
+    },
+    xAxis: {
+      ...AxisCategory,
+      axisTick: {
+        show: true,
+        alignWithLabel: true
+      },
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: lineColor,
+          width: 1,
+          type: 'dashed'
+        }
+      },
+      data: datas.xAxis,
+      boundaryGap: false
+    },
+    yAxis: {
+      data: datas.yAxis,
+      ...AxisCategory,
+      axisTick: {
+        show: true,
+        alignWithLabel: true
+      },
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: lineColor,
+          width: 1,
+          type: 'dashed'
+        }
+      }
+    },
+    series: [{
+      name: '观看时长',
+      type: 'scatter',
+      symbolSize: function (val) {
+        return val[2] * 60 / maxValue
+      },
+      data: datas.data,
+      animationDelay: function (idx) {
+        return idx * 5
+      }
+    }]
   }
   let myChart = echarts.init(document.getElementById(id))
   myChart.setOption(option)
