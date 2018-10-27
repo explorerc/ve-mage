afbddd8d3b73aa5eebeb543ca04733a6af785a10
quchan <chan.qu@vhall.com>
2018-10-27 15:01:00 +0800

    u

---
+++ src/api/userManage-service.js
@@ -0,0 +1,16 @@
+export default {
+  // 批量倒入接口 传文件
+  POST_GROUP_IMPORT: '/common/group/user-import',
+  // 查询分组
+  GET_GROUP_LIST: '/common/group/all-lists',
+  // 查询标签
+  GET_TAG_LIST: '/common/tag/all-lists',
+  // 查询所有活动
+  GET_ACTIVE_LIST: '/manage/base-manage/list-activity',
+  // 用户池查询
+  GET_USERS_POOL: '/user/customer/lists',
+  // 添加到群组
+  POST_ADD_TO_GROUP: '/common/group/user-add',
+  // 删除成员
+  POST_DEL_USERS: '/common/group/user-delete'
+}
