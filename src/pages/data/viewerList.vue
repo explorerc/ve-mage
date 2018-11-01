5c054861e6a0661c0b5f2b16f12492e36bab48d3
yyg <yongguan.yang@vhall.com>
2018-11-01 13:39:52 +0800

    观众列表

--- src/pages/data/viewerList.vue
+++ src/pages/data/viewerList.vue
@@ -171,7 +171,7 @@
 
   export default {
     name: 'viewerList',
-    components: {VePagination},
+    components: { VePagination },
     data () {
       return {
         isHigh: false,
@@ -196,54 +196,54 @@
           pageSize: 10
         },
         genderList: [
-          {value: '', label: '全部'},
-          {value: 'M', label: '男'},
-          {value: 'W', label: '女'}
+          { value: '', label: '全部' },
+          { value: 'M', label: '男' },
+          { value: 'W', label: '女' }
         ],
         watcherTypeList: [
-          {value: '', label: '全部用户'},
-          {value: 1, label: '优质用户'},
-          {value: 2, label: '潜在用户'},
-          {value: 3, label: '一般用户'},
-          {value: 4, label: '高价值用户'},
-          {value: 5, label: '流失用户'}
+          { value: '', label: '全部用户' },
+          { value: 1, label: '优质用户' },
+          { value: 2, label: '潜在用户' },
+          { value: 3, label: '一般用户' },
+          { value: 4, label: '高价值用户' },
+          { value: 5, label: '流失用户' }
         ],
         provinceList: [
-          {value: '', label: '省'},
-          {value: 1, label: '北京'},
-          {value: 2, label: '河南省'},
-          {value: 3, label: '河北省'},
-          {value: 4, label: '黑龙江'},
-          {value: 5, label: '湖北'}
+          { value: '', label: '省' },
+          { value: 1, label: '北京' },
+          { value: 2, label: '河南省' },
+          { value: 3, label: '河北省' },
+          { value: 4, label: '黑龙江' },
+          { value: 5, label: '湖北' }
         ],
         cityList: [
-          {value: '', label: '市'},
-          {value: 1, label: '北京市'},
-          {value: 2, label: '郑州市'},
-          {value: 3, label: '天津市'}
+          { value: '', label: '市' },
+          { value: 1, label: '北京市' },
+          { value: 2, label: '郑州市' },
+          { value: 3, label: '天津市' }
         ],
         tradeList: [
-          {value: '', label: '全部'},
-          {value: 1, label: '导入'},
-          {value: 2, label: '微信注册'},
-          {value: 3, label: 'PC注册'}
+          { value: '', label: '全部' },
+          { value: 1, label: '导入' },
+          { value: 2, label: '微信注册' },
+          { value: 3, label: 'PC注册' }
         ],
         deviceList: [
-          {value: '', label: '全部'},
-          {value: 'pc', label: '电脑'},
-          {value: 'phone', label: '手机'}
+          { value: '', label: '全部' },
+          { value: 'pc', label: '电脑' },
+          { value: 'phone', label: '手机' }
         ],
         scoreTypeList: [
-          {value: '', label: '全部'},
-          {value: 1, label: '100>得分>90'},
-          {value: 2, label: '90>得分>80'},
-          {value: 3, label: '80>得分>60'},
-          {value: 4, label: '60>得分'}
+          { value: '', label: '全部' },
+          { value: 1, label: '100>得分>90' },
+          { value: 2, label: '90>得分>80' },
+          { value: 3, label: '80>得分>60' },
+          { value: 4, label: '60>得分' }
         ],
         userTypeList: [
-          {value: '', label: '全部'},
-          {value: 0, label: '老用户'},
-          {value: 1, label: '新用户'}
+          { value: '', label: '全部' },
+          { value: 0, label: '老用户' },
+          { value: 1, label: '新用户' }
         ]
       }
     },
