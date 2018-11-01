59292aea7171f716129b09999ac3796d9fbb5e25
yao.lu <yao.lu@vhall.com>
2018-11-01 18:18:06 +0800

    群组

--- src/pages/sales-tools/recommend-goods/info.vue
+++ src/pages/sales-tools/recommend-goods/info.vue
@@ -15,9 +15,14 @@
         <el-input v-model="goodsData.name"></el-input>
       </el-form-item>
       <el-form-item label="商品描述">
-        <ve-upload title="图片小于2MB 支持jpg、png、bmp格式 最佳尺寸：600 x 600" accept="png|jpg|jpeg|bmp" :defaultImg="defaultImg"
-                   :fileSize="2048" :errorMsg="uploadImgErrorMsg" @error="uploadError"
-                   @success="uploadImgSuccess"></ve-upload>
+        <div class="upload_box">
+          <ve-upload v-for="(ite,ind) in upload_list" title="图片小于2MB 支持jpg、png、bmp格式 最佳尺寸：600 x 600"
+                     accept="png|jpg|jpeg|bmp" :defaultImg="defaultImg" :nowIndex="ind || 0"
+                     :fileSize="2048" :errorMsg="uploadImgErrorMsg" @error="uploadError"
+                     @success="uploadImgSuccess"></ve-upload>
+          <el-button @click="add_upload" v-if="upload_list.length<4">+</el-button>
+        </div>
+
       </el-form-item>
       <el-form-item label="商品链接">
         <el-input v-model="goodsData.name" type="url"></el-input>
@@ -34,7 +39,8 @@
 </template>
 
 <script>
-  import VeUpload from 'src/components/ve-upload-image'
+  // import VeUpload from 'src/components/ve-upload-image'
+  import VeUpload from 'src/components/ve-upload-group'
 
   export default {
     name: 'info',
@@ -51,6 +57,7 @@
     },
     data () {
       return {
+        upload_list: [{}],
         goodsData: {
           name: ''
         },
@@ -58,6 +65,9 @@
       }
     },
     methods: {
+      add_upload () {
+        this.upload_list.push({})
+      },
       onSubmit () {
       },
       uploadImgSuccess (data) {
@@ -73,5 +83,24 @@
 <style lang="scss" scoped>
   #goods-info {
     padding: 40px 100px;
+    /deep/ {
+      .upload_box {
+        display: flex;
+        .ve-upload-box {
+          margin: auto 20px;
+          display: inline-block;
+          .com-upload {
+            width: 100%;
+            .upload-file-box {
+              width: 100%;
+              span {
+                display: inline-block;
+                margin: auto 20px;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 </style>
