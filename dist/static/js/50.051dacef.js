622f5fd2285eaaa84db69704ee98acd7abad1733
xiaoguang.hu <xiaoguang.hu@vhall.com>
2018-11-09 22:41:51 +0800

    修复分组bug

---
+++ dist/static/js/50.051dacef.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{1024:function(n,t,e){"use strict";e.r(t);var i={},s=(e(991),e(2)),u=Object(s.a)(i,function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("红包雨")])},[],!1,null,null,null);u.options.__file="index.vue";t.default=u.exports},656:function(n,t,e){},991:function(n,t,e){"use strict";var i=e(656);e.n(i).a}}]);
