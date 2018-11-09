8d78f3d35da4302f5241c2218cb1627b2552c394
quchan <chan.qu@vhall.com>
2018-11-09 21:49:42 +0800

    u

---
+++ dist/static/js/52.3bd07a1f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{639:function(n,t,e){},842:function(n,t,e){"use strict";var i=e(639);e.n(i).a},867:function(n,t,e){"use strict";e.r(t);var i={},s=(e(842),e(2)),u=Object(s.a)(i,function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("红包雨")])},[],!1,null,null,null);u.options.__file="index.vue";t.default=u.exports}}]);
