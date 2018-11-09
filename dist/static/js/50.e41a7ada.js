a7c0675d941a9656be4780f86ca9b3a1f8c57214
yyg <yongguan.yang@vhall.com>
2018-11-09 21:44:31 +0800

    build test

---
+++ dist/static/js/50.e41a7ada.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{639:function(t,e,n){},842:function(t,e,n){"use strict";var s=n(639);n.n(s).a},867:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("红包雨")])};s._withStripped=!0;var i={},r=(n(842),n(2)),a=Object(r.a)(i,s,[],!1,null,null,null);a.options.__file="src/pages/sales-tools/redpack/index.vue";e.default=a.exports}}]);
+//# sourceMappingURL=50.e41a7ada.js.map
