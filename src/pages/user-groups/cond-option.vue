e90c7bba9262982c9b5076a95ed6706cff1e0db8
yao.lu <yao.lu@vhall.com>
2018-11-09 18:46:03 +0800

    群组

--- src/pages/user-groups/cond-option.vue
+++ src/pages/user-groups/cond-option.vue
@@ -698,12 +698,12 @@
                 type: 'date',
                 unit: ''
               }, {
-                key: 'gt',
+                key: 'lt',
                 name: '早于（哪一天）',
                 type: 'date',
                 unit: ''
               }, {
-                key: 'lt',
+                key: 'gt',
                 name: '晚于（哪一天）',
                 type: 'date',
                 unit: ''
@@ -728,12 +728,12 @@
                 type: 'date',
                 unit: ''
               }, {
-                key: 'gt',
+                key: 'lt',
                 name: '早于（哪一天）',
                 type: 'date',
                 unit: ''
               }, {
-                key: 'lt',
+                key: 'gt',
                 name: '晚于（哪一天）',
                 type: 'date',
                 unit: ''
