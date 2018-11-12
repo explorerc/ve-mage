a56d4120875a97b1ba5748ac617d7ef72a24374b
jinpeng <peng.jin@vhall.com>
2018-11-12 20:55:44 +0800

    build test

---
+++ dist/static/js/50.d033bdd3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{640:function(t,e,n){},843:function(t,e,n){"use strict";var s=n(640);n.n(s).a},868:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("红包雨")])};s._withStripped=!0;var i={},r=(n(843),n(2)),a=Object(r.a)(i,s,[],!1,null,null,null);a.options.__file="src/pages/sales-tools/redpack/index.vue";e.default=a.exports}}]);
+//# sourceMappingURL=50.d033bdd3.js.map
