f1442b5245de2a04113cf05ed2d56aa410cee972
yyg <yongguan.yang@vhall.com>
2018-11-14 14:38:16 +0800

    build test

--- dist/static/js/52.c4782596.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{640:function(t,e,n){},845:function(t,e,n){"use strict";var s=n(640);n.n(s).a},908:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("红包雨")])};s._withStripped=!0;var i={},r=(n(845),n(2)),a=Object(r.a)(i,s,[],!1,null,null,null);a.options.__file="src/pages/sales-tools/redpack/index.vue";e.default=a.exports}}]);
-//# sourceMappingURL=52.c4782596.js.map
