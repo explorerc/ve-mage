0fb6b61e8e2c8602158211292ef0901f8d628ccf
jinpeng <peng.jin@vhall.com>
2018-12-27 17:59:57 +0800

    build prod

--- dist/static/js/50.a96582d7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{504:function(n,t,e){"use strict";e.r(t);var i={},s=(e(917),e(1)),u=Object(s.a)(i,function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("红包雨")])},[],!1,null,null,null);u.options.__file="index.vue";t.default=u.exports},707:function(n,t,e){},917:function(n,t,e){"use strict";var i=e(707);e.n(i).a}}]);
